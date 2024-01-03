c10604238ff47b9ddf6d2312bd46a1fb054299cb
jiezhou <1747364257@qq.com>
2024-01-03 11:42:25 +0800

    Gee-cache lru 实现

---
+++ Gee-Cache/geecache/lru/lru.go
@@ -0,0 +1,70 @@
+package lru
+
+import (
+	"container/list"
+)
+
+type Cache struct {
+	maxBytes  int64                         //最大使用内存
+	nBytes    int64                         //已使用内存
+	ll        *list.List                    //双向链表
+	cache     map[string]*list.Element      // key string   value 双向链表中对应的节点指针
+	OnEvicted func(key string, value Value) //某条记录被删除时的回调函数
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+// Value use Len to count how many bytes it takes
+type Value interface {
+	Len() int
+}
+
+// New is the constructor of cache
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+// Get look uos a key value
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// RemoveOldest removes the oldest item
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// Add adds a value to the cache
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nBytes += int64(value.Len()) - int64(value.Len())
+	}
+
+	for c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+}
