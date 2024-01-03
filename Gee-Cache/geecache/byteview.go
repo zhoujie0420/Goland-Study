f657c885562184bbd9f6e7ac110246d8a31f57cb
jiezhou <1747364257@qq.com>
2024-01-03 15:26:14 +0800

    Gee-cache 单机的并发缓存

---
+++ Gee-Cache/geecache/byteview.go
@@ -0,0 +1,25 @@
+package geecache
+
+type ByteView struct {
+	b []byte
+}
+
+// Len returns the view length
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+// String returns the data as a string
+func (v ByteView) string() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
